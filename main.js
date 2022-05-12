1e408ff5a9b75d11bbb41fccf845db9537cd612c
karanvsingh02 <86027389+karanvsingh02@users.noreply.github.com>
2022-05-12 18:15:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initailized!");
+}
+
+function draw(){
+    background("#808080");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function draw(){
+    background('#6C91C2');
+
+    textSize(difference);
+    fill('#FFE787');
+    text('KARAN', 50, 400);
+}
